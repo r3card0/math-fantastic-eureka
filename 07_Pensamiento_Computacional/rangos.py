2404807657b7b956775641e609535ebc831bce04
r3card0 <ricardo.rdz@pm.me>
2022-03-26 13:49:05 -0600

    Create a loop of odd numbers

---
+++ 07_Pensamiento_Computacional/rangos.py
@@ -0,0 +1,11 @@
+def description():
+    """This program generates range of odd numbers from 1 to 99
+    """
+
+description()
+
+def odd_numbers():
+    for i in range(1,100,2):
+        print(i)
+    
+odd_numbers()
